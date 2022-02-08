fb6537f808ef003e642d77fea26cea431105e845
Serkova Ann <88327097+sashkovna@users.noreply.github.com>
2022-02-08 16:17:52 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename='/countries_spa_app'>
             <App/>
         </BrowserRouter>
     </React.StrictMode>,
