63f10df7e63c113a74af4bf74fe652220dbbc14d
Anna <any9684@rambler.ru>
2022-02-08 15:42:51 +0300

    project complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Main from "./components/Main";
+import {Route, Routes} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Details from "./pages/Details";
+import NotFound from "./pages/NotFound";
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [countries, setCountries] = useState([])
+
+    return (
+        <>
+            <Header/>
+            <Main>
+                <Routes>
+                    <Route path="/" element={<HomePage countries={countries} setCountries={setCountries}/>}/>
+                    <Route path="/country/:name" element={<Details/>}/>
+                    <Route path="/*" element={<NotFound/>}/>
+                </Routes>
+            </Main>
+        </>
+    );
 }
 
 export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,74 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import {ALL_COUNTRIES} from "../config";
+import Controls from "../components/Controls";
+import List from "../components/List";
+import Card from "../components/Card";
+import {useNavigate} from "react-router-dom";
+
+const HomePage = ({countries, setCountries}) => {
+    const [filteredCountries, setFilteredCountries] = useState(countries)
+    const navigate = useNavigate()
+
+    const handleSearch = (search, region) => {
+        let data = [...countries]
+
+        if (region) {
+            data = data.filter(c => c.region.includes(region))
+        }
+
+        if (search) {
+            data = data.filter(c => c.name.toLowerCase().includes(search.toLowerCase()))
+        }
+        setFilteredCountries(data)
+    }
+
+    useEffect(() => {
+        if(!countries.length)
+        axios.get(ALL_COUNTRIES)
+            .then(({data}) => setCountries(data))
+        // eslint-disable-next-line
+    }, [])
+
+    useEffect(() => {
+        handleSearch()
+        // eslint-disable-next-line
+    }, [countries])
+
+    return (
+        <div>
+            <Controls onSearch={handleSearch}/>
+            <List>
+                {filteredCountries.map(c => {
+                    const countryInfo = {
+                        img: c.flags.png,
+                        name: c.name,
+                        info: [
+                            {
+                                title: 'Population',
+                                description: c.population.toLocaleString()
+                            },
+                            {
+                                title: 'Region',
+                                description: c.region
+                            },
+                            {
+                                title: 'Capital',
+                                description: c.capital
+                            }
+                        ]
+                    }
+                    return (
+                    <Card
+                        key={c.name}
+                        {...countryInfo}
+                        onClick={() => navigate(`/country/${c.name}`)}
+                    />)
+                })}
+            </List>
+
+        </div>
+    );
+};
+
+export default HomePage;
