01b54ad544309ddd6f6006da090d4718f01ce946
Anna <any9684@rambler.ru>
2022-02-08 16:34:58 +0300

    change basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter basename='/countries-spa-app'>
+        <BrowserRouter basename='/countries_spa_app'>
             <App/>
         </BrowserRouter>
     </React.StrictMode>,
